b64ed8aaa42cb1a773bdd6511baefb547bc9775d
Lamine <Mlamine.abdellaoui@aucegypt.edu>
2023-07-09 15:56:14 +0100

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,24 +6,24 @@ const siteMetadata = {
   title: 'Best Nextjs Tailwind Portfolio',
   description:
     'This is where you can learn about me and my work. I am a full stack developer and I love to build things.',
-  author: 'Curtis Warcup',
-  authorHeadline: 'Full Stack Developer, amateur pizza critic 🍕',
+  author: 'Lamine Abdellaoui',
+  authorHeadline: 'Sotfware Engineer, ECM consultant and I help people find visa sponsored jobs',
   authorAbout:
     "Hi, I'm Curtis, a full stack developer. I created this to help my mom create a website for all her crazy business ideas. I hope you like it!",
   authorAboutExtended:
     "Curtis is a Canadian-born, living in beautify Vancouver, BC. \n \n In his personal life, Curtis is an avid cyclist and health nut. He enjoys making weekly meal preps and running with his dog. When he's not coding, exercising, or cooking, Curtis can be found in various mechanical keyboard enthusiast forums, scoping out his next purchase. I also like eating food, especially pizza.",
   socials: {
-    twitter: 'https://twitter.com/curtiswarcup',
-    github: 'https://github.com/Cwarcup',
-    linkedin: 'https://www.linkedin.com/in/curtiswarcup/',
-    instagram: 'https://www.instagram.com/curtiswarcup/',
-    facebook: 'https://www.facebook.com/curtiswarcup',
+    twitter: 'https://twitter.com/Lamiiiiiine',
+    github: 'https://github.com/Lamiiine',
+    linkedin: 'https://www.linkedin.com/in/mlamine08/',
+//    instagram: 'https://www.instagram.com/curtiswarcup/',
+//    facebook: 'https://www.facebook.com/curtiswarcup',
   },
-  email: 'curtis.gwarcup@gmail.com',
-  phoneNumber: '123-456-7890',
+  email: 'mlamine.abdellaoui@aucegypt.edu',
+  //phoneNumber: '123-456-7890',
   contactTitle: 'Get in touch',
   contactSubtitle:
-    "I am currently making banging pizza's and amazing websites. If you want to get in touch, please use the form below.",
+    "I am currently working as an ECM consultant. If you want to get in touch, please use the form below.",
   analytics: {
     plausibleDataDomain: 'https://cwarcup.com/', // e.g. tailwind-nextjs-starter-blog.vercel.app
     googleAnalyticsId: 'G-XXXXXXX', // e.g. UA-000000-2 or G-XXXXXXX
